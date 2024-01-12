bdef93610fa48d60323b14274ac2f6ce0ebffd4f
josejulianalvarezu@gmail.com <joejulianalvarezu@gmail.com>
2024-01-12 08:43:25 -0500

    Primer Imput

---
+++ js/SaboresDelMundo.js
@@ -0,0 +1,7 @@
+// function mostrarCuadroRojo() {
+//     document.getElementById("cuadro").style.backgroundColor = "red";
+//   }
+
+//   function mostrarCuadroAzul() {
+//     document.getElementById("cuadro").style.backgroundColor = "blue";
+//   }
